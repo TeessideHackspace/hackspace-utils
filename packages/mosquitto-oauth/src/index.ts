779596a11af3127f98cb0d1ae4c6523f73a62ea8
Jim Allanson <jim@amplience.com>
2020-10-05 21:39:25 +0100

    fix: fix this binding in mosquitto-oauth

--- packages/mosquitto-oauth/src/index.ts
+++ packages/mosquitto-oauth/src/index.ts
@@ -10,8 +10,8 @@ const authService = new AuthService(TOKEN_ENDPOINT);
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/user', authService.userHandler);
-app.post('/superuser', authService.superuserHandler);
-app.post('/acl', authService.aclhandler);
+app.post('/user', (req, res) => authService.userHandler(req, res));
+app.post('/superuser', (req, res) => authService.superuserHandler(req, res));
+app.post('/acl', (req, res) => authService.aclhandler(req, res));
 
 app.listen(PORT);
